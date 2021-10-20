ab664423081942b8fee437e40dd9a9d0ca62b701
alienwithoutfriend <68382753+alienwithoutfriend@users.noreply.github.com>
2021-10-20 12:05:06 +0700

    commit

---
+++ app/Http/Controllers/CobaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Friends;
+
+
+class CobaController extends Controller
+{
+    /* public function index()
+    {
+        return 'test berhasil';
+    }
+
+    public function urutan($ke)
+    {
+        $friends = Friends::paginate(3);
+        return view('friend', compact('friends'));
+    }
+
+    public function coba($ke)
+    {
+        return view('coba', ['ke' => $ke]);
+    }*/
+    public function index()
+    {
+        $friends = Friends::paginate(3);
+        return view('index', compact('friends'));
+    }
+
+    public function create()
+    {
+
+        return view('create');
+    }
+    public function store(Request $request)
+    {
+        // Validate the request...
+
+        $friends = new Friends;
+
+        $friends->nama = $request->nama;
+        $friends->no_tlp = $request->no_tlp;
+        $friends->alamat = $request->alamat;
+
+        $friends->save();
+    }
+}
